db2a131313e030df0c3d442f7b010ee09ab250ae
priyanka gupta <pynk1992@gmail.com>
2023-08-26 00:07:42 +0200

    answer button done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const bookmarkImg = document.querySelector('[data-js="bookmarkImg"]');
-const bookmarkButton = document.querySelector('[data-js="bookmarkButton"]');
+const bookmark = document.querySelector('[data-js="bookmarkButton"]');
 
 function addBlackImage() {
   bookmarkImg.setAttribute("src", "./images/bookmark.png");
@@ -9,10 +9,25 @@ function addWhiteImage() {
   bookmarkImg.setAttribute("src", "./images/whitebookmark.png");
 }
 
-bookmarkButton.addEventListener("click", () => {
-  if (bookmarkImg.src === "http://localhost:5500/images/whitebookmark.png") {
+bookmark.addEventListener("click", () => {
+  if (bookmarkImg.classList.contains("bookmarked")) {
+    bookmarkImg.classList.remove("bookmarked");
     addBlackImage();
   } else {
+    bookmarkImg.classList.add("bookmarked");
     addWhiteImage();
+  }
+});
+
+const answerButton = document.querySelector('[data-js="answer-btn"]');
+const answerText = document.querySelector('[data-js="answer-text"]');
+
+answerButton.addEventListener("click", () => {
+  if (answerText.classList.contains("hidden")) {
+    answerText.classList.remove("hidden");
+    answerButton.textContent = "hide answer";
+  } else {
+    answerText.classList.add("hidden");
+    answerButton.textContent = "show answer";
   }
 });
